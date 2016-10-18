00e4ff7c6f58aae9935dec6c57520dfcfe28cc23
peng8350 <83508440@qq.com>
2016-10-18 12:33:22 +0800

    init

--- app/src/main/java/com/peng/frescoprogress/MainActivity.java
+++ app/src/main/java/com/peng/frescoprogress/MainActivity.java
@@ -2,6 +2,7 @@ package com.peng.frescoprogress;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.QwertyKeyListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        QwertyKeyListener listener;
     }
 }
