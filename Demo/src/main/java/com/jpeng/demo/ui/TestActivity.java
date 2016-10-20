6c097cb6b2f71a823763f89f6685f011baf9ed23
peng8350 <83508440@qq.com>
2016-10-20 23:14:45 +0800

    gitls

---
+++ Demo/src/main/java/com/jpeng/demo/ui/TestActivity.java
@@ -0,0 +1,7 @@
+package com.jpeng.demo.ui;
+
+/**
+ * Created by peng on 16-10-20.
+ */
+public class TestActivity {
+}
--- Fresco-progress/src/main/java/com/jpeng/progress/CircleStyle.java
+++
@@ -1,8 +0,0 @@
-package com.jpeng.progress;
-
-/**
- * Created by peng on 16-10-18.
- */
-public enum CircleStyle {
-	RING, FAN
-}
